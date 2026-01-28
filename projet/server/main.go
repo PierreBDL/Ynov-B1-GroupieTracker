9d4b2c3e2f0c5529c4ff2d3281d90db9e3596db6
PierreBDL <delavergnepierre@gmail.com>
2026-01-28 13:45:56 +0100

    Hébergement sur Scalingo

--- projet/server/main.go
+++ projet/server/main.go
@@ -79,7 +79,7 @@ func main() {
 		"/assets/",
 		http.StripPrefix(
 			"/assets/",
-			http.FileServer(http.Dir("projet/front/assets")),
+			http.FileServer(http.Dir("../front/assets")),
 		),
 	)
 
@@ -113,7 +113,7 @@ func main() {
 
 	// credits
 	http.HandleFunc("/credits", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("projet/front/templates/credits.html")
+		tmpl, err := template.ParseFiles("../front/templates/credits.html")
 		if err != nil {
 			log.Println("Template error:", err)
 			http.Error(w, "Template not found", 500)
@@ -138,7 +138,7 @@ func main() {
 
 // Afficher l'écran d'accueil
 func afficherAccueil(w http.ResponseWriter, data donnees) {
-	tmpl, err := template.ParseFiles("projet/front/templates/index.html")
+	tmpl, err := template.ParseFiles("../front/templates/index.html")
 	if err != nil {
 		log.Println("Template error:", err)
 		http.Error(w, "Template not found", 500)
@@ -182,7 +182,7 @@ func afficherArtiste(w http.ResponseWriter, data donnees, id int) {
 		Relation: donneesRelation,
 	}
 
-	tmpl, err := template.ParseFiles("projet/front/templates/artist.html")
+	tmpl, err := template.ParseFiles("../front/templates/artist.html")
 	if err != nil {
 		log.Println("Template error:", err)
 		http.Error(w, "Template not found", 500)
